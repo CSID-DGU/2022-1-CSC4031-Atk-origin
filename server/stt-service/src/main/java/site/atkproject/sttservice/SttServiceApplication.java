ef5364b13d03243ab5318bcc90831b6e98258617
Seokju Hong <iamseokjuhong@gmail.com>
2022-03-26 21:55:47 +0900

    [추가] gitignore 내용 추가 및 스프링 부트 프로젝트 추가

---
+++ server/stt-service/src/main/java/site/atkproject/sttservice/SttServiceApplication.java
@@ -0,0 +1,13 @@
+package site.atkproject.sttservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SttServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SttServiceApplication.class, args);
+    }
+
+}
---
+++ server/stt-service/src/test/java/site/atkproject/sttservice/SttServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package site.atkproject.sttservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SttServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
