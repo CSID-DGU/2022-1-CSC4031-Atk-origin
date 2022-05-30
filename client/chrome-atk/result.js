7a1f74b5593c83c8c3de9a7d1aeebdd0b5eb7563
heesuju <hsjoo2442@gmail.com>
2022-05-31 01:52:33 +0900

    [추가] 답안 확인 화면 추가

--- client/chrome-atk/result.js
+++ client/chrome-atk/result.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
   const backButton = document.getElementById('back');
   const logo = document.getElementById("logo");
   const resultMsg = document.getElementById("resultMsg");
+  const answerLink = document.getElementById("answerLink");
 
   logo.onclick = () => {chrome.tabs.create({url: "https://github.com/CSID-DGU/2022-1-CSC4031-Atk-origin"})};
-  
   backButton.onclick = () => {window.location.href="popup.html"};
-
+  answerLink.onclick = () => {window.location.href="answersheet.html"};
+  
   const urlParams = new URLSearchParams(window.location.search);
   const total = urlParams.get('total');
   const correct = urlParams.get('correct');
